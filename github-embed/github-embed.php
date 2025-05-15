57fa1299a8645869edc6c552da480edf3bc3f593
Sean Murphy <sean@iamseanmurphy.com>
2025-05-15 16:19:37 +0000

    2.2.1

--- github-embed/github-embed.php
+++ github-embed/github-embed.php
@@ -3,13 +3,13 @@
 Plugin Name: Github Embed
 Plugin URI: https://wordpress.org/plugins/github-embed/
 Description: Paste the URL to a Github project into your posts or pages, and have the project information pulled in and displayed automatically
-Version: 2.1.0
+Version: 2.2.1
 Author: Ademti Software Ltd.
 Author URI: https://www.ademti-software.co.uk/
 */
 
 /**
- * Copyright (c) 2013 Ademti Software. All rights reserved.
+ * Copyright (c) 2013-2025 Ademti Software. All rights reserved.
  *
  * Released under the GPL license v2
  * https://www.gnu.org/licenses/gpl-2.0.en.html
@@ -188,7 +188,7 @@ class github_embed {
 	 */
 	private function process_template( $template, $data ) {
 		ob_start();
-		if ( ! locate_template( 'wp-github-oembed/' . $template, true ) ) {
+		if ( ! locate_template( 'wp-github-oembed/' . $template, true, false, ['data' => $data] ) ) {
 			require_once 'templates/' . $template;
 		}
 
